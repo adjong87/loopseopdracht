5ce45a3075c67ff820cefaa38476509ad3c25bb1
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-03-22 23:12:17 +0100

    Add third exercise

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -26,3 +26,27 @@ const numbers = [2, 4, 5, 29, 38];
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
+
+
+// ==========================================
+// Opdracht 3
+// Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
+// Het volume is lengte x breedte x hoogte
+// Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
+// ==========================================
+
+const squares = [30, 2, 8, 24, 11];
+
+// Verwachtte uitkomsten:
+// Vóór het script zie je de originele waardes:
+// console.log(numbers) geeft: [30, 2, 8, 24, 11];
+// Na jouw script zie je de aangepaste waardes:
+// console.log(numbers) geeft: [27000, 8, 512, 13824, 1331];
+
+console.log(squares);
+
+for (let i = 0; i < squares.length; i++) {
+  squares[i] = squares[i] * squares[i] * squares[i];
+}
+
+console.log(squares);
