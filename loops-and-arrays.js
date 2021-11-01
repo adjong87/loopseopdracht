d4face9a2d5bf9c830c6d4e1897ad4eed0c86da8
novaeeken <nova_eeken@hotmail.com>
2021-11-01 13:49:15 +0100

    Update loops-and-arrays

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -1,6 +1,6 @@
 // ==========================================
 // Opdracht 1
-// Schrijf een script dat iedere naam in bovenstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
+// Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
 
@@ -13,9 +13,11 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
 
+
+
 // ==========================================
 // Opdracht 2
-// Schrijf een script dat ieder getal in bovenstaande array met 3 vermenigvuldigd.
+// Schrijf een script dat ieder EVEN getal in onderstaande array met 2 vermenigvuldigd, en ieder ONEVEN getal met 3
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
@@ -25,12 +27,15 @@ const numbers = [2, 4, 5, 29, 38];
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
-// console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
+// console.log(numbers) geeft: [ 4, 12, 10, 87, 76 ];
+
+
 
 
 // ==========================================
 // Opdracht 3
 // Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
+// Vervolgens moet de huidige waarde in de array overschreven worden met: "Het volume van [x] is [y]"
 // Het volume is lengte x breedte x hoogte
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
@@ -41,4 +46,10 @@ const squares = [30, 2, 8, 24, 11];
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [30, 2, 8, 24, 11];
 // Na jouw script zie je de aangepaste waardes:
-// console.log(numbers) geeft: [27000, 8, 512, 13824, 1331];
+// console.log(numbers) geeft: [
+//   'Het volume van 30 is 27000',
+//   'Het volume van 2 is 8',
+//   'Het volume van 8 is 512',
+//   'Het volume van 24 is 13824',
+//   'Het volume van 11 is 1331'
+// ]
