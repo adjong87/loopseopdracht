c4799ea8e87b5955cc165e1013087126d0279df3
adjong87 <adjong87@gmail.com>
2022-03-28 16:58:10 +0200

    Inlever versie

--- loops.js
+++ loops.js
@@ -5,7 +5,9 @@
 // Hoera!
 // Hoera!
 // ==========================================
-
+for (let i = 0; i<3;i++){
+    console.log("Hoera!")
+}
 
 
 // ==========================================
@@ -16,8 +18,11 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
-
+// ==========================================
+for(let i = 0; i < 4;i++){
+    console.log("loop...")
+}
+console.log("Klaar!")
 
 
 // ==========================================
@@ -32,8 +37,13 @@
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
 // ==========================================
-
-
+for(let i = 1 ; i <= 15;i++)
+    if (i%2==0){
+        console.log(i + " is even")
+    }
+    else{
+        console.log(i + " is oneven")
+    }
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -50,4 +60,11 @@
 // >> 8
 // >> 9
 // ==========================================
-
+for(let i = 0;i<10;i++)
+    if(i >=3 && i <= 5){
+        console.log("> " + i)
+    } else if (i >=6 && i <=9){
+        console.log(">> " + i)
+    } else {
+        console.log(i)
+    }
